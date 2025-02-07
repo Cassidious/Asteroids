5f2f60941db1d27bb5e2d4b7b8386a2410d19c90
Cassidious <hendra725@gmail.com>
2025-02-07 15:58:13 -0500

    added game loop

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,6 +24,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
